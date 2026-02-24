e64524b390ce731871d7afbbff2ad5d445aac1f3
xiashuidaolaoshuren <so1490600850@gmail.com>
2026-02-24 12:44:16 +0800

    feat: update minimum SDK version to 30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.xiashuidaolaoshuren.allergyguard"
-        minSdk = 24
+        minSdk = 30
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
